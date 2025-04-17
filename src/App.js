e2a1c91eb6570149cad242838ad916d23f5d18a1
vuanhp265 <vuanhp265@gmail.com>
2025-04-17 19:53:53 +0700

    vuanhp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import ClassDetail from './components/ClassDetail';
+import ClassDetailone from './components/ClassDetailone';
 import Level0Table from './components/Level0Table';
 import Level1Table from './components/Level1Table';
-import ClassDetailone from './components/ClassDetailone';
-import CourseTable from './components/Coursetable'; // ✅ import
+import CourseTable from './components/Coursetable';
+import AboutUs from './components/AboutUs'; // 👉 Bạn cần tạo thêm component này
 
 import './App.css';
 
@@ -12,23 +14,30 @@ function App() {
   return (
     <Router>
       <div className="App">
+        {/* Navbar */}
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link className="navbar-brand" to="/">Lịch Khai Giảng</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Tất cả các lớp</Link>
-              </li>
-              <li className="nav-item"> 
-                <Link className="nav-link" to="/courses">Courses</Link>
-              </li>
-            </ul>
+          <div className="container">
+            <Link className="navbar-brand" to="/">Lịch Khai Giảng</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Tất cả các lớp</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/courses">Courses</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">About Us</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
 
+        {/* Main Content */}
         <div className="container mt-4">
           <Routes>
             <Route path="/" element={
@@ -39,7 +48,8 @@ function App() {
             } />
             <Route path="/class/:id" element={<ClassDetail />} />
             <Route path="/class1/:id" element={<ClassDetailone />} />
-            <Route path="/courses" element={<CourseTable />} /> {/* ✅ Route mới */}
+            <Route path="/courses" element={<CourseTable />} />
+            <Route path="/about" element={<AboutUs />} />
           </Routes>
         </div>
       </div>
